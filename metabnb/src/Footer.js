510861b7bd885b1a2d367e65c6415da998a6d51a
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-11-15 22:00:06 +0300

    feat: footer-elements

--- metabnb/src/Footer.js
+++ metabnb/src/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {TbBrandFacebook} from "react-icons/tb";
 import {TbBrandInstagram} from "react-icons/tb";
-import {TfiTwitter} from "react-icons/tf";
+import {SlSocialTwitter} from "react-icons/sl";
 
 
 
@@ -11,7 +11,22 @@ function Footer(){
         <img className= "dark" src="/black.png" alt="propt"/> <h2>Metabnb</h2>
         <TbBrandFacebook />
         <TbBrandInstagram />
-        <TfiTwitter />
+        <SlSocialTwitter />
+        <img className="at" src="/at.png" alt="mark"/>
+        <ul className="lister">
+            <li><h2>Community</h2></li>
+            <li>NFT</li>
+            <li>Tokens</li>
+            <li>Landlords</li>
+            <li>Discord</li>
+        </ul>
+        <ul className="row">
+            <li><h2>Places</h2></li>
+            <li>Castle</li>
+            <li>Farms</li>
+            <li>Beach</li>
+            <li>Learn more</li>
+        </ul>
         </>
     )
 }
