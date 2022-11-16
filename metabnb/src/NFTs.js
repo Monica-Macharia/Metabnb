feaadecf12b497996fdd27d229720ad5941e02e9
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-11-16 10:02:06 +0300

    style:nft

--- metabnb/src/NFTs.js
+++ metabnb/src/NFTs.js
@@ -5,9 +5,10 @@ function NFTs(){
         <div className= "nft">
             <h2 className ="nf">Metabnb NFTs</h2>
             <p className= "discover">Discover our NFT gift cards collection. Loyal<br/> customers gets amazing gift cards which are <br/> traded as NFTs. These NFTs gives our customer <br/> access to loads of our exclusive services.</p>
-            <button className= "learning">Learn more</button>
+          
             <img className="three" src="/see.png" alt="three" />
-        </div>
+         <button className= "learning">Learn more</button>
+         <h1 className="hide">6</h1> </div>
 
     )
 }
