96e56d593629bb40628ecc2169eaa5e5ad6d3e9b
Monica-Macharia <monicamacharia7@gmail.com>
2023-12-08 13:16:49 +0300

    community

--- metabnb/src/App.js
+++ metabnb/src/App.js
@@ -6,19 +6,20 @@ import PlaceToStay from "./PlaceToStay";
 import NFTs from "./NFTs.js";
 import Community from "./Community.js";
 import NavBar from './NavBar';
-import Footer from './Footer';
+// import Footer from './Footer';
 
 function App() {
   return (
     <main>
-      {<NavBar />}
+      <NavBar />
       <Routes>
+        
         <Route path="/" element={<Home />}/>
         <Route path="/PlaceToStay" element={<PlaceToStay />} />
         <Route path="/NFTs" element={<NFTs/>}/>
         <Route path="/Community" element={<Community/>}/>
       </Routes>
-      {<Footer />}
+      
     </main>
   )
 }
--- metabnb/src/Community.js
+++ metabnb/src/Community.js
@@ -1,10 +1,91 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function Community(){
-    return(
-        <>
-    <p id="hide">4</p>
-        </>
-    )
-}
+
+
+
+const Community = () => {
+
+    const [data, setData] = useState([]);
+
+   
+
+    useEffect(() => {
+        
+        fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+        .then(res => res.json())
+        .then(data => {setData(data.meals);
+            
+        } )
+    }, [])
+    console.log(data);
+
+    return (
+        <div class="card mb-3" >
+            <h2>Community Cafe</h2>
+              {data.map((image, index)=>(
+        <div class="row g-0">
+          <div class="col-md-4">
+           
+            <img key={index} class="center" src={image.strMealThumb} class="img-fluid rounded-start" alt={image.strMeal} />
+
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{image.strMeal}</h5>
+              <p class="card-text"> {image.strInstructions}</p>
+             
+            </div>
+          </div>
+        </div>
+        ))}
+          
+      </div>
+
+
+
+
+
+
+    //  <div class=""> 
+
+    //   <div class="col" className="container">
+    //     <h2>Community Cafe</h2>
+    //       {data.map((image, index)=>(
+            
+    //           <div class="card">
+    //            { console.log(image.strMealThumb)}
+    //            <img key={index} class="center" src={image.strMealThumb} class="card-img-top" alt={image.strMeal} />
+    //            <div class="card-body">
+    //             <h5 class="card-title">{image.strMeal}</h5>
+    //             <p class="card-text">{image.strInstructions}</p>
+    //           </div>
+            
+    //         </div>      
+    //       ))}
+          
+
+
+                   
+                 
+    //             </div>
+        
+        
+    //     </div>
+
+            // <div class="row row-cols-1 row-cols-md-2 g-4">
+            // <div class="col">
+            // {data.map((image, index)=>( 
+            // <div class="card">
+            //     <img key={index} src="{image.strMealThumb}" class="card-img-top" alt="{image.strMeal}" />
+            //     <div class="card-body">
+            //     <h5 class="card-title">Card title</h5>
+            //     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            //     </div>
+            // </div>))}
+            // /div>
+            // </div>
+
+        );
+        
+};
 export default Community;
